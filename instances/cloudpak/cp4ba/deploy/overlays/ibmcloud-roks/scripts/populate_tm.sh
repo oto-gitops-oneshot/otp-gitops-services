f989e4e6c1316824f8f27aa9c321abc71e57a477
Jeff Tan <jeffetan@au1.ibm.com>
2022-08-24 11:07:57 +1000

    fix: Clarify messages to avoid confusion as to env vars, which are not required in these newer versions. Also make it so that failure to find the target key to patch does not lead to abnormal exit, particularly in case multiple target files for which some have the target key and some do not.

--- instances/cloudpak/cp4ba/deploy/overlays/ibmcloud-roks/scripts/populate_tm.sh
+++ instances/cloudpak/cp4ba/deploy/overlays/ibmcloud-roks/scripts/populate_tm.sh
@@ -7,8 +7,8 @@
 function populate_value()
 {
     if [[ -z ${POP_KEY} ]] || [[ -z ${POP_VAL} ]] || [[ -z ${POP_FILE} ]] ; then
-        echo "Error: Incomplete input env vars."
-        echo "Please provide all three of the expected env var names: POP_KEY, POP_VAL, and POP_FILE."
+        echo "Error: Incomplete input vars."
+        echo "Please provide all three of the expected vars: POP_KEY, POP_VAL, and POP_FILE."
         echo "This script populates the key with the given value in the named file."
         exit 1
     fi
@@ -25,12 +25,10 @@ function populate_value()
 
     if [ $(grep -c ${POP_KEY} ${POP_FILE}) -eq 0 ] ; then
         echo "Warning: target key '${POP_KEY}' not found in given file '${POP_FILE}'. Nothing to do."
-        exit 0
+    else
+        # This form of sed should work on both Linux (GNU sed) and Darwin/MacOS.
+        sed -i'.bak' -e 's/'${POP_KEY}:' .*/'${POP_KEY}': "'${POP_VAL}'"/' $POP_FILE && rm ${POP_FILE}.bak
     fi
-
-    # This form of sed should work on both Linux (GNU sed) and Darwin/MacOS.
-    sed -i'.bak' -e 's/'${POP_KEY}:' .*/'${POP_KEY}': "'${POP_VAL}'"/' $POP_FILE && rm ${POP_FILE}.bak
-    
 }
 
 ## Main
